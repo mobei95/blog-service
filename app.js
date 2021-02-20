5891cf5af2bdeb8ea5d8d33aa6adf79923094386
subingcheng <subingcheng@avglife.cn>
2021-02-20 16:45:37 +0800

    update: 更新eslint的配置

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
-const router = require('./router')
+const express = require('express');
+const app = express();
+const router = require('./router');
 
-app.use('/', router)
+app.use('/', router);
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 app.listen('9990', () => {
-	console.log('服务启动')
-})
+	console.log('服务启动');
+});

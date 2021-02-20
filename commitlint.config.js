38a3e762b7506584d4625f6638151550711e9469
subingcheng <subingcheng@avglife.cn>
2021-02-20 14:08:30 +0800

    update: 修改commitlint配置文件

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,20 +1,18 @@
-/**
-* feature：新功能
-* update：更新某功能
-* fix：修补某功能的bug
-* refactor：重构某个功能
-* optimize: 优化构建工具或运行时性能
-* style：仅样式改动
-* docs：仅文档新增/改动
-* chore：构建过程或辅助工具的变动
-*/
+'use strict';
 module.exports = {
   extends: [
     '@commitlint/config-conventional'
   ],
   rules: {
     'type-enum': [2, 'always', [
-      'feature', 'update', 'fix', 'refactor', 'optimize', 'style', 'docs', 'chore'
+      'feature',  // 新功能
+      'update', // 更新某功能
+      'fix',  // 修补某功能的bug
+      'refactor',  // 重构某个功能
+      'optimize', // 优化构建工具或运行时性能
+      'style', // 仅样式改动
+      'docs', // 仅文档新增/改动
+      'chore' // 构建过程或辅助工具的变动
     ]],
     'type-case': [0],
     'type-empty': [0],
@@ -22,6 +20,6 @@ module.exports = {
     'scope-case': [0],
     'subject-full-stop': [0, 'never'],
     'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72]
+    'header-max-length': [0, 'always', 100]
   }
 }
